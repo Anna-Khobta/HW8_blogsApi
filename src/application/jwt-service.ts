ca14605fbdc5b85e0ef0d436a8bc8d7eedde7ab4
Anna-Khobta <ana14i88@gmail.com>
2023-03-25 01:24:42 +0300

    10s, 20s

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -9,8 +9,8 @@ import {tokenRepositories} from "../repositories/token-db-repositories";
 export const jwtService = {
     async createJwtToken(foundUserInDb: UserDbType) {
 
-        const accessToken = jwt.sign({userId: foundUserInDb.id}, settings.JWT_SECRET, {expiresIn: '10h'})
-        const refreshToken = jwt.sign({userId: foundUserInDb.id}, settings.JWT_SECRET, {expiresIn: '1h'})
+        const accessToken = jwt.sign({userId: foundUserInDb.id}, settings.JWT_SECRET, {expiresIn: '10s'})
+        const refreshToken = jwt.sign({userId: foundUserInDb.id}, settings.JWT_SECRET, {expiresIn: '20s'})
 
         const jwtResult = {accessToken: accessToken, refreshToken: refreshToken}
         return jwtResult
@@ -41,8 +41,8 @@ export const jwtService = {
                 return null
             } else {
 
-                const newAccessToken = jwt.sign({userId: decodedRefreshToken.userId}, settings.JWT_SECRET, {expiresIn: '10h'})
-                const newRefreshToken = jwt.sign({userId: decodedRefreshToken.id}, settings.JWT_SECRET, {expiresIn: '1h'})
+                const newAccessToken = jwt.sign({userId: decodedRefreshToken.userId}, settings.JWT_SECRET, {expiresIn: '10s'})
+                const newRefreshToken = jwt.sign({userId: decodedRefreshToken.id}, settings.JWT_SECRET, {expiresIn: '20s'})
 
                 const addOldTokenInDb =  await revokedTokenService.createToken(refreshToken)
                 // так можно ??
